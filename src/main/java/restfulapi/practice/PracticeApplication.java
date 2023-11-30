76dbc34118727d5a5e9f7eaa99198453987a0bd4
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-11-30 15:29:09 +0700

    create bean for model mapper

--- src/main/java/restfulapi/practice/PracticeApplication.java
+++ src/main/java/restfulapi/practice/PracticeApplication.java
@@ -2,6 +2,8 @@ package restfulapi.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 public class PracticeApplication {
@@ -10,4 +12,8 @@ public class PracticeApplication {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
